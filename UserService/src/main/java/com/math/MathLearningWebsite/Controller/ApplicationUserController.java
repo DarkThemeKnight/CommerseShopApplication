2f96a9caa3069c0e03839148d24c53e98b76c80c
DarkThemeKnight <ayanfeoluwadafidi@outlook.com>
2023-12-31 18:44:22 +0100

    second

---
+++ UserService/src/main/java/com/math/MathLearningWebsite/Controller/ApplicationUserController.java
@@ -0,0 +1,45 @@
+package com.math.MathLearningWebsite.Controller;
+
+import com.math.MathLearningWebsite.Services.Jwt.JwtService;
+import com.math.MathLearningWebsite.Services.previleges.customer.ApplicationUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/user/control/v1")
+public class ApplicationUserController {
+    private final JwtService jwtService;
+    private final ApplicationUserService applicationUserService;
+    @PutMapping("/address")
+    public ResponseEntity<String> updateAddress(@RequestHeader("Authorization") String authorizationHeader, @RequestParam String address) {
+        String jwtToken = jwtService.extractTokenFromHeader(authorizationHeader);
+        String result = applicationUserService.updateAddress(jwtToken, address);
+        return ResponseEntity.ok(result);
+    }
+    @PutMapping("/password")
+    public ResponseEntity<String> updatePassword(@RequestHeader("Authorization") String authorizationHeader, @RequestParam String newPassword) {
+        String jwtToken = jwtService.extractTokenFromHeader(authorizationHeader);
+        String result = applicationUserService.updatePassword(jwtToken, newPassword);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/phoneNumber")
+    public ResponseEntity<String> updatePhoneNumber(@RequestHeader("Authorization") String authorizationHeader, @RequestParam String phoneNumber) {
+        String jwtToken = jwtService.extractTokenFromHeader(authorizationHeader);
+        String result = applicationUserService.updatePhoneNumber(jwtToken, phoneNumber);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/security")
+    public ResponseEntity<String> updateSecurityQuestion(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @RequestParam String securityQuestion,
+            @RequestParam String securityAnswer
+    ) {
+        String jwtToken = jwtService.extractTokenFromHeader(authorizationHeader);
+        String result = applicationUserService.updateSecurityQuestion(jwtToken, securityQuestion, securityAnswer);
+        return ResponseEntity.ok(result);
+    }
+}
--- UserService/src/main/java/com/math/MathLearningWebsite/Services/previleges/customer/ApplicationUserService.java
+++ UserService/src/main/java/com/math/MathLearningWebsite/Services/previleges/customer/ApplicationUserService.java
@@ -1,12 +1,15 @@
 package com.math.MathLearningWebsite.Services.previleges.customer;
 
+import com.math.MathLearningWebsite.Services.Jwt.JwtService;
 import com.math.MathLearningWebsite.dao.ApplicationUserDao;
 import com.math.MathLearningWebsite.entity.ApplicationUser;
+import com.math.MathLearningWebsite.enumerations.Message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,6 +20,10 @@ import java.util.Optional;
 public class ApplicationUserService implements UserDetailsService {
     @Autowired
     private final ApplicationUserDao userDao;
+    @Autowired
+    private final JwtService jwtService;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
     @Override
     public ApplicationUser loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<ApplicationUser> applicationUser = userDao.findByEmail(username);
@@ -27,6 +34,52 @@ public class ApplicationUserService implements UserDetailsService {
         log.debug("User found");
         return applicationUser.get();
     }
+    public String updateAddress(String jwtToken, String address){
+        String username = jwtService.extractMail(jwtToken); // Extract username from token
+        ApplicationUser user = userDao.findByEmail(username).orElse(null); // Retrieve user from database
+        if (user != null) {
+            user.setAddress(address); // Update user's address
+            userDao.save(user); // Save updated user details to the database
+            return "Address updated successfully";
+        } else {
+            return Message.USER_NOT_FOUND.getMessage();
+        }
+    }
 
+    public String updatePassword(String jwtToken, String newPassword){
+        String username = jwtService.extractMail(jwtToken); // Extract username from token
+        ApplicationUser user = userDao.findByEmail(username).orElse(null); // Retrieve user from database
+
+        if (user != null) {
+            user.setPassword(passwordEncoder.encode(newPassword)); // Encode and set new password
+            userDao.save(user); // Save updated user details to the database
+            return "Password updated successfully";
+        } else {
+            return Message.USER_NOT_FOUND.getMessage();
+        }
+    }
+    public String updatePhoneNumber(String jwtToken, String phoneNumber){
+        String username = jwtService.extractMail(jwtToken); // Extract username from token
+        ApplicationUser user = userDao.findByEmail(username).orElse(null); // Retrieve user from database
+        if (user != null) {
+            user.setPhoneNumber(phoneNumber); // Update user's phone number
+            userDao.save(user); // Save updated user details to the database
+            return "Phone number updated successfully";
+        } else {
+            return "User not found";
+        }
+    }
+    public String updateSecurityQuestion(String jwtToken, String securityQuestion, String securityAnswer){
+        String username = jwtService.extractMail(jwtToken); // Extract username from token
+        ApplicationUser user = userDao.findByEmail(username).orElse(null); // Retrieve user from database
+        if (user != null) {
+            user.setSecurityQuestion(securityQuestion); // Update user's security question
+            user.setSecurityAnswer(securityAnswer); // Update user's security answer
+            userDao.save(user); // Save updated user details to the database
+            return "Security question and answer updated successfully";
+        } else {
+            return "User not found";
+        }
+    }
 
 }
